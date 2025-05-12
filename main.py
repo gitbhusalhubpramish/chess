830521188d98cc7dc01c15cad2d17acb7bff20e2
pramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2025-05-12 10:41:31 +0000

    Restored to 'ef643471bee7ecbfde766ac17a024ea91f7d4096'
    Replit-Restored-To: ef643471bee7ecbfde766ac17a024ea91f7d4096

--- main.py
+++ main.py
@@ -28,32 +28,32 @@ def draw_board(WHITE, ROWS, COLS, SQUARE_SIZE, BLACK):
 # Load chess pieces
 player1 = player("white")
 wk = [pygame.image.load("white player/king.png") for _ in range(player1.characters["king"]["no"])]
-wk = [pygame.transform.scale(wk, (100, 100)) for wk in wk]
+wk = [pygame.transform.scale(wk, (70, 70)) for wk in wk]
 wq = [pygame.image.load("white player/queen.png") for _ in range(player1.characters["queen"]["no"])]
 wr = [pygame.image.load("white player/rook.png") for _ in range(player1.characters["rook"]["no"])]
 wb = [pygame.image.load("white player/bishop.png") for _ in range(player1.characters["bishop"]["no"])]
 wn = [pygame.image.load("white player/knight.png") for _ in range(player1.characters["knight"]["no"])]
 wp = [pygame.image.load("white player/pawn.png") for _ in range(player1.characters["pawn"]["no"])]
-wq = [pygame.transform.scale(wq, (100, 100)) for wq in wq]
-wr = [pygame.transform.scale(wr, (100, 100)) for wr in wr]
-wb = [pygame.transform.scale(wb, (100, 100)) for wb in wb]
-wn = [pygame.transform.scale(wn, (100, 100)) for wn in wn]
-wp = [pygame.transform.scale(wp, (100, 100)) for wp in wp]
+wq = [pygame.transform.scale(wq, (70, 70)) for wq in wq]
+wr = [pygame.transform.scale(wr, (70, 70)) for wr in wr]
+wb = [pygame.transform.scale(wb, (70, 70)) for wb in wb]
+wn = [pygame.transform.scale(wn, (70, 70)) for wn in wn]
+wp = [pygame.transform.scale(wp, (70, 70)) for wp in wp]
 
 
 player2 = player("black")
 bk = [pygame.image.load("black player/king.png") for _ in range(player2.characters["king"]["no"])]
-bk = [pygame.transform.scale(bk, (100, 100)) for bk in bk]
+bk = [pygame.transform.scale(bk, (70, 70)) for bk in bk]
 bq = [pygame.image.load("black player/queen.png") for _ in range(player2.characters["queen"]["no"])]
 br = [pygame.image.load("black player/rook.png") for _ in range(player2.characters["rook"]["no"])]
 bb = [pygame.image.load("black player/bishop.png") for _ in range(player2.characters["bishop"]["no"])]
 bn = [pygame.image.load("black player/knight.png") for _ in range(player2.characters["knight"]["no"])]
 bp = [pygame.image.load("black player/pawn.png") for _ in range(player2.characters["pawn"]["no"])]
-bq = [pygame.transform.scale(bq, (100, 100)) for bq in bq]
-br = [pygame.transform.scale(br, (100, 100)) for br in br]
-bb = [pygame.transform.scale(bb, (100, 100)) for bb in bb]
-bn = [pygame.transform.scale(bn, (100, 100)) for bn in bn]
-bp = [pygame.transform.scale(bp, (100, 100)) for bp in bp]
+bq = [pygame.transform.scale(bq, (70, 70)) for bq in bq]
+br = [pygame.transform.scale(br, (70, 70)) for br in br]
+bb = [pygame.transform.scale(bb, (70, 70)) for bb in bb]
+bn = [pygame.transform.scale(bn, (70, 70)) for bn in bn]
+bp = [pygame.transform.scale(bp, (70, 70)) for bp in bp]
 
 chess_positions = {}
 
@@ -75,28 +75,28 @@ def drawpcs():
         'K': wk[0], 'Q': wq[0], 'R': wr[0], 'B': wb[0], 'N': wn[0], 'P': wp[0],  # White pieces
         'k': bk[0], 'q': bq[0], 'r': br[0], 'b': bb[0], 'n': bn[0], 'p': bp[0]   # Black pieces
     }
-
+    
     # Create board representation from player positions
     board = [['' for _ in range(8)] for _ in range(8)]
-
+    
     # Map positions for both players
     for player in [player1, player2]:
         is_white = player.color == "white"
         piece_chars = {'king': 'K', 'queen': 'Q', 'rook': 'R', 
                       'bishop': 'B', 'knight': 'N', 'pawn': 'P'}
-
+        
         for piece_type, piece_data in player.characters.items():
             char = piece_chars[piece_type]
             if not is_white:
                 char = char.lower()
-
+                
             for piece in piece_data["detail"]:
                 if piece["alive"]:
                     pos = piece["position"]
                     col = ord(pos[0]) - ord('a')
                     row = 8 - int(pos[1])
                     board[row][col] = char
-
+    
     # Draw pieces
     for row in range(8):
         for col in range(8):
@@ -115,16 +115,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-
+    
     # Clear screen and draw board first
     draw_board((255, 255, 255), 8, 8, 70, (0, 0, 0))
-
+    
     # Draw pieces on top of board
     drawpcs()
-
+    
     # Update display
     pygame.display.flip()
-
+    
     # Update display
     pygame.display.update()
 
