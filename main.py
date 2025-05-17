bbe8e39985a2efd0935edbd627324ca55579a648
pramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2025-05-17 10:28:21 +0000

    possible valid posotion

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import pygame
 from players import player
 import platform
+from moves import knight_moves, king_moves, rook_moves, bishop_moves, queen_moves, pawn_moves
 
 if platform.system() == "Linux":
     os.environ["SDL_VIDEODRIVER"] = "x11"
@@ -20,6 +21,7 @@ def draw_board(WHITE, ROWS, COLS, SQUARE_SIZE, BROWN):
                     screen, BROWN,
                     (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
                 )
+                
 player1 = player("white")
 wk = [pygame.image.load("white player/king.png") for _ in range(player1.characters["king"]["no"])]
 wq = [pygame.image.load("white player/queen.png") for _ in range(player1.characters["queen"]["no"])]
@@ -53,42 +55,43 @@ original_pcs = {
 
 def whtrsz(siz):
     for i in range(8):
-        wp[i] = pygame.transform.scale(wp[i], (siz, siz))
+        wp[i] = pygame.transform.smoothscale(original_pcs["P"][i], (siz, siz))
     for i in range(2):
-        wr[i] = pygame.transform.scale(wr[i], (siz, siz))
-        wb[i] = pygame.transform.scale(wb[i], (siz, siz))
-        wn[i] = pygame.transform.scale(wn[i], (siz, siz))
-    wq[0] = pygame.transform.scale(wq[0], (siz, siz))
-    wk[0] = pygame.transform.scale(wk[0], (siz, siz))
+        wr[i] = pygame.transform.smoothscale(original_pcs["R"][i], (siz, siz))
+        wb[i] = pygame.transform.smoothscale(original_pcs["B"][i], (siz, siz))
+        wn[i] = pygame.transform.smoothscale(original_pcs["N"][i], (siz, siz))
+    wq[0] = pygame.transform.smoothscale(original_pcs["Q"][0], (siz, siz))
+    wk[0] = pygame.transform.smoothscale(original_pcs["K"][0], (siz, siz))
+
 
 def blkrsz(siz):
     for i in range(8):
-        bp[i] = pygame.transform.scale(bp[i], (siz, siz))
+        bp[i] = pygame.transform.smoothscale(original_pcs["p"][i], (siz, siz))
     for i in range(2):
-        br[i] = pygame.transform.scale(br[i], (siz, siz))
-        bb[i] = pygame.transform.scale(bb[i], (siz, siz))
-        bn[i] = pygame.transform.scale(bn[i], (siz, siz))
-    bq[0] = pygame.transform.scale(bq[0], (siz, siz))
-    bk[0] = pygame.transform.scale(bk[0], (siz, siz))
+        br[i] = pygame.transform.smoothscale(original_pcs["r"][i], (siz, siz))
+        bb[i] = pygame.transform.smoothscale(original_pcs["b"][i], (siz, siz))
+        bn[i] = pygame.transform.smoothscale(original_pcs["n"][i], (siz, siz))
+    bq[0] = pygame.transform.smoothscale(original_pcs["q"][0], (siz, siz))
+    bk[0] = pygame.transform.smoothscale(original_pcs["k"][0], (siz, siz))
 
 def rsz(siz, psc, idx):
     pcs[psc][idx] = pygame.transform.smoothscale(original_pcs[psc][idx], (siz, siz))
 
 def dcrzall():
     for i in range(8):
-        wp[i] = pygame.transform.scale(wp[i], (65, 65))
-        bp[i] = pygame.transform.scale(bp[i], (65, 65))
+        wp[i] = pygame.transform.smoothscale(original_pcs["P"][i], (65, 65))
+        bp[i] = pygame.transform.smoothscale(original_pcs["p"][i], (65, 65))
     for i in range(2):
-        wr[i] = pygame.transform.scale(wr[i], (65, 65))
-        wb[i] = pygame.transform.scale(wb[i], (65, 65))
-        wn[i] = pygame.transform.scale(wn[i], (65, 65))
-        br[i] = pygame.transform.scale(br[i], (65, 65))
-        bb[i] = pygame.transform.scale(bb[i], (65, 65))
-        bn[i] = pygame.transform.scale(bn[i], (65, 65))
-    wq[0] = pygame.transform.scale(wq[0], (65, 65))
-    wk[0] = pygame.transform.scale(wk[0], (65, 65))
-    bq[0] = pygame.transform.scale(bq[0], (65, 65))
-    bk[0] = pygame.transform.scale(bk[0], (65, 65))
+        wr[i] = pygame.transform.smoothscale(original_pcs["R"][i], (65, 65))
+        wb[i] = pygame.transform.smoothscale(original_pcs["B"][i], (65, 65))
+        wn[i] = pygame.transform.smoothscale(original_pcs["N"][i], (65, 65))
+        br[i] = pygame.transform.smoothscale(original_pcs["r"][i], (65, 65))
+        bb[i] = pygame.transform.smoothscale(original_pcs["b"][i], (65, 65))
+        bn[i] = pygame.transform.smoothscale(original_pcs["n"][i], (65, 65))
+    wq[0] = pygame.transform.smoothscale(original_pcs["Q"][0], (65, 65))
+    wk[0] = pygame.transform.smoothscale(original_pcs["K"][0], (65, 65))
+    bq[0] = pygame.transform.smoothscale(original_pcs["q"][0], (65, 65))
+    bk[0] = pygame.transform.smoothscale(original_pcs["k"][0], (65, 65))
 
 
 whtrsz(70)
@@ -104,12 +107,20 @@ chess_positions = {
     }
     for row in range(8) for col in range(8)
 }
+packedsqr = []
+def ckcpcn():
+    for player_obj in [player1, player2]:
+        for p_type, p_data in player_obj.characters.items():
+            for idx, piece in enumerate(p_data["detail"]):
+                if piece["alive"]:
+                    packedsqr.append(piece["position"])
 
 piece_position_map = {}
 pcs = {
     "K": wk, "Q": wq, "R": wr, "B": wb, "N": wn, "P": wp,
     "k": bk, "q": bq, "r": br, "b": bb, "n": bn, "p": bp
 }
+pic = {"K": "king", "Q": "queen", "R": "rook", "B": "bishop", "N": "knight", "P": "pawn"}
 
 def drawpcs():
     piece_position_map.clear()
@@ -129,6 +140,21 @@ def drawpcs():
                     screen.blit(pcs[char][idx], (x, y))
                     piece_position_map[pos] = (char, idx)
 
+def get_moves(char, pos, color):
+    if char.upper() == 'K':
+        return king_moves(pos)
+    elif char.upper() == 'Q':
+        return queen_moves(pos)
+    elif char.upper() == 'R':
+        return rook_moves(pos)
+    elif char.upper() == 'B':
+        return bishop_moves(pos)
+    elif char.upper() == 'N':
+        return knight_moves(pos)
+    elif char.upper() == 'P':
+        return pawn_moves(pos, color)
+    return []
+
 clock = pygame.time.Clock()
 running = True
 Selected = None
@@ -136,6 +162,7 @@ wt = True
 
 while running:
     for event in pygame.event.get():
+        nextmv = None
         if event.type == pygame.QUIT:
             running = False
 
@@ -147,6 +174,10 @@ while running:
             rank = str(8 - row)
             square = file + rank
 
+            if Selected is not None:
+                ckcpcn()
+                nextmv = square if (square not in packedsqr and square in get_moves(Selected[0], player1.characters[pic[Selected[0]]]["detail"][Selected[1]]["position"] if wt else player2.characters[pic[Selected[0]]]["detail"][Selected[1]]["position"], "white" if wt else "black")) else None
+            print(nextmv)
             if square in piece_position_map:
                 Selected = piece_position_map[square]
             else:
@@ -161,6 +192,10 @@ while running:
         elif Selected is not None and wt and Selected[0].isupper():
             dcrzall()
             rsz(85, Selected[0], Selected[1])
+        elif Selected is not None and not wt and Selected[0].islower():
+            dcrzall()
+            rsz(85, Selected[0], Selected[1])
+        
 
     draw_board((255, 255, 255), 8, 8, 70, (150, 75, 0))
     drawpcs()
